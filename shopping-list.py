b859b6114dc3bbc7056301c6d2dcdd770ec582ff
Agnieszka Falandys <agnfal@gmail.com>
2024-07-22 11:11:31 +0200

    iterating over the dictionary elements has been created

--- shopping-list.py
+++ shopping-list.py
@@ -2,3 +2,7 @@ shoppingList = {
   "Piekarnia" : ['Chleb', 'Pączek', 'Bułki'],
   "Warzywniak" : ['Marchew', 'Seler', 'Rukola']
 }
+count = 0;
+for shop, products in shoppingList.items():
+  print(f"Idę do {shop.upper()} i kupuję tam następujące rzeczy: {[p.upper() for p in products]}")
+  count = count + len(products)
